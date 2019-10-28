0052089c7b46e8a1fa39067917c36f5ad58e2a9b
Alexander Albert <alexander@vilkrig.co.nz>
2019-10-28 17:07:16 +0800

    added dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
+const path = require(`path`)
 
 module.exports = {
   plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `assets/images`),
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
