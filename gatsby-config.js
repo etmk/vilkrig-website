6d454691aa5703001c9385a14bafd44688e188ed
Alexander Albert <alexander@vilkrig.co.nz>
2019-10-28 16:56:58 +0800

    added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,12 @@
 
 module.exports = {
-
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_TRACKING_ID,
+        cookieDomain: "vilkrig.co.nz",
+      },
+    },
+  ],
 }
