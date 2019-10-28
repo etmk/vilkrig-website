6446becfc2bf6d455832256fcb25bc2b84487afa
Alexander Albert <alexander@vilkrig.co.nz>
2019-10-28 18:28:30 +0800

    refactoring

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import SEO from "../components/seo/seo"
+
+const AboutPage = () => (
+  <main>
+    <SEO title="About" description="We are a small team with a big ❤️" />
+    <h1>About</h1>
+  </main>
+)
+
+export default AboutPage
