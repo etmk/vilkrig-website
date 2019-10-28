9841b97520d3a3c875b0c7fa08a2437a8a98f9d7
Alexander Albert <alexander@vilkrig.co.nz>
2019-10-28 18:21:34 +0800

    added SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,36 @@
 const path = require(`path`)
 
 module.exports = {
+  siteMetadata: {
+    title: `Vilkrig`,
+    siteUrl: `https://www.vilkrig.co.nz`,
+    description: `Digital agency from New Zealand 🇳🇿`,
+    author: `Vilkrig Limited`,
+  },
   plugins: [
     `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: path.join(__dirname, `src`, `assets/images`),
+        path: `${__dirname}/src/assets/images`,
       },
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Vilkrig Limited`,
+        short_name: `vilkrig`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#004156`,
+        display: `standalone`,
+        icon: `${__dirname}/src/assets/images/icon.png`,
+      },
+    },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
