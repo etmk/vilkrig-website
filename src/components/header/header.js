6e8cbe10f32bb10f936e3f8d146cb9124bf0440b
Alexander Albert <alexander@vilkrig.co.nz>
2019-10-28 22:44:15 +0800

    layout changes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -21,74 +21,76 @@ class Header extends Component {
     let menuClass = this.state.open[3] ? styles.toggleOpen : styles.toggleClosed
     return (
       <header>
-        <div className={styles.logo}>
-          <Link to="/">
-            <Logo />
-          </Link>
-        </div>
-
-        <nav>
-          <ul className={styles.navItems + " " + menuClass}>
-            <li className={styles.navItem}>
-              <Link
-                to="/"
-                onClick={this.handleClick.bind(this, 3)}
-                activeClassName={styles.activeNavItem}
-              >
-                Home
-              </Link>
-            </li>
-            <li className={styles.navItem}>
-              <Link
-                to="/projects"
-                onClick={this.handleClick.bind(this, 3)}
-                activeClassName={styles.activeNavItem}
-              >
-                Projects
-              </Link>
-            </li>
-            <li className={styles.navItem}>
-              <Link
-                to="/services"
-                onClick={this.handleClick.bind(this, 3)}
-                activeClassName={styles.activeNavItem}
-              >
-                Services
-              </Link>
-            </li>
-            <li className={styles.navItem}>
-              <Link
-                to="/about"
-                onClick={this.handleClick.bind(this, 3)}
-                activeClassName={styles.activeNavItem}
-              >
-                About
-              </Link>
-            </li>
-            <li className={styles.navItem}>
-              <Link
-                to="/contact"
-                onClick={this.handleClick.bind(this, 3)}
-                activeClassName={styles.activeNavItem}
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
-          <div className={styles.mobileNav}>
-            <HamburgerMenu
-              isOpen={this.state.open[3]}
-              menuClicked={this.handleClick.bind(this, 3)}
-              width={22}
-              height={16}
-              strokeWidth={3}
-              rotate={0}
-              color="black"
-              borderRadius={2}
-              animationDuration={0.75}
-            />
+        <div className={styles.headerGrid}>
+          <div className={styles.logo}>
+            <Link to="/">
+              <Logo />
+            </Link>
           </div>
-        </nav>
+
+          <nav>
+            <ul className={styles.navItems + " " + menuClass}>
+              <li className={styles.navItem}>
+                <Link
+                  to="/"
+                  onClick={this.handleClick.bind(this, 3)}
+                  activeClassName={styles.activeNavItem}
+                >
+                  Home
+                </Link>
+              </li>
+              <li className={styles.navItem}>
+                <Link
+                  to="/projects"
+                  onClick={this.handleClick.bind(this, 3)}
+                  activeClassName={styles.activeNavItem}
+                >
+                  Projects
+                </Link>
+              </li>
+              <li className={styles.navItem}>
+                <Link
+                  to="/services"
+                  onClick={this.handleClick.bind(this, 3)}
+                  activeClassName={styles.activeNavItem}
+                >
+                  Services
+                </Link>
+              </li>
+              <li className={styles.navItem}>
+                <Link
+                  to="/about"
+                  onClick={this.handleClick.bind(this, 3)}
+                  activeClassName={styles.activeNavItem}
+                >
+                  About
+                </Link>
+              </li>
+              <li className={styles.navItem}>
+                <Link
+                  to="/contact"
+                  onClick={this.handleClick.bind(this, 3)}
+                  activeClassName={styles.activeNavItem}
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+            <div className={styles.mobileNav}>
+              <HamburgerMenu
+                isOpen={this.state.open[3]}
+                menuClicked={this.handleClick.bind(this, 3)}
+                width={22}
+                height={16}
+                strokeWidth={3}
+                rotate={0}
+                color="black"
+                borderRadius={2}
+                animationDuration={0.75}
+              />
+            </div>
+          </nav>
+        </div>
       </header>
     )
   }
